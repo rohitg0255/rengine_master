9d133216e165a1be1bb8453425a2c1800ba74232
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-11-09 13:21:04 +0530

    message

--- web/dashboard/models.py
+++ web/dashboard/models.py
@@ -9,19 +9,21 @@ class SearchHistory(models.Model):
 
 
 class Project(models.Model):
-	id = models.AutoField(primary_key=True)
-	name = models.CharField(max_length=500)
-	slug = models.SlugField(unique=True)
-	insert_date = models.DateTimeField()
-	limit = models.IntegerField(default=0)
-	domains = models.ManyToManyField('Domain', related_name='domains')
-	notification = models.ForeignKey(Notification, on_delete=models.CASCADE, related_name="notification")
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=500)
+    slug = models.SlugField(unique=True)
+    insert_date = models.DateTimeField()
+    limit = models.IntegerField(default=0)
+    domains = models.ManyToManyField("Domain", related_name="domains")
+    notification = models.ForeignKey(
+        Notification, on_delete=models.CASCADE, related_name="notification"
+    )
 
     def __str__(self):
         return self.slug
 
-	def get_domains(self):
-		return Domain.objects.filter(project__in=Project.objects.filter(id=self.id))
+    def get_domains(self):
+        return Domain.objects.filter(project__in=Project.objects.filter(id=self.id))
 
 
 class OpenAiAPIKey(models.Model):

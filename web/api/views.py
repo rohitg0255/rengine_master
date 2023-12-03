b43515839e764580ca5864f7d375583a97cc8926
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-12-03 12:06:55 +0530

    mdd

--- web/api/views.py
+++ web/api/views.py
@@ -141,7 +141,7 @@ class Summary(APIView):
 
             # Domain
             # target = get_object_or_404(Domain, id=id)
-            target = Domain.objects.select_related("domain_info").all().get(id=id)
+            target = Domain.objects.select_related("domain_info__set").get(id=id)
             context["target"] = model_to_dict(target)
 
             # Scan History

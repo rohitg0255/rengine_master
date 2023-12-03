3eb3cb8dfcdb4cbde0f70375c09b64fc991035fb
Rohit Gurkhe <rohitgurkhe@gmail.com>
2023-12-03 12:35:33 +0530

    mdd

--- web/api/views.py
+++ web/api/views.py
@@ -143,7 +143,7 @@ class Summary(APIView):
             # target = get_object_or_404(Domain, id=id)
             target = Domain.objects.prefetch_related("domain_info").get(id=id)
             context["target"] = model_to_dict(target)
-            domain_info = DomainInfo.object.get(id=target.domain_info)
+            domain_info = DomainInfo.objects.get(id=target.domain_info)
             context["domain_info"] = model_to_dict(domain_info)
             # try:
             #     context["domain_info"] = target.domain_info
